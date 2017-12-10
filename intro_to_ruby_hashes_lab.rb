6a8ced08282c8f0757e4bdff4c552b1fe7789ab9
Robert Watson <spartan83@gmail.com>
2017-12-10 23:03:11 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,8 +17,8 @@ def monopoly_with_second_tier
   monopoly = {}
   monopoly[:railroads]={}
   monopoly[:railroads]={pieces: 4}
-  monopoly[:names]={}
-  monopoly[:rent_in_dollars]={}
+  monopoly[:railroads]={names: {}}
+  monopoly[:railroads]={rent_in_dollars: {}}
   monopoly
 end
 
