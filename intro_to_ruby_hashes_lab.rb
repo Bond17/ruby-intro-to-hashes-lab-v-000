f433a3892bd871074f36045f077363dfdeb70759
Robert Watson <spartan83@gmail.com>
2017-12-10 23:10:01 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,7 +19,6 @@ def monopoly_with_second_tier
   monopoly[:railroads][:pieces]= 4
   monopoly[:railroads][:names]= {}
   monopoly[:railroads][:rent_in_dollars]= {}
-  puts monopoly
   monopoly
 end
 
@@ -34,7 +33,6 @@ def monopoly_with_third_tier
   monopoly[:railroads][:rent_in_dollars][:two_pieces_owned]=50
   monopoly[:railroads][:rent_in_dollars][:three_pieces_owned]=100
   monopoly[:railroads][:rent_in_dollars][:four_pieces_owned]=200
-  puts monopoly
   monopoly
 
 
